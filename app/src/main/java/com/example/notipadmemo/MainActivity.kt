ccd6c5f5de3b4fd20544d440da507918d04c1836
XavierRivera05 <126819995+XavierRivera05@users.noreply.github.com>
2025-10-11 13:44:18 -0600

    Inicio de aplicación (prototipo feo)
    el que no baila no come bizcoho 🗣🗣🗣

--- app/src/main/java/com/example/notipadmemo/MainActivity.kt
+++ app/src/main/java/com/example/notipadmemo/MainActivity.kt
@@ -16,17 +16,7 @@ import com.example.notipadmemo.ui.theme.NotipadMemoTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            NotipadMemoTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
+        setContentView(R.layout.activity_main) //de aquí se carga el layout
     }
 }
 
